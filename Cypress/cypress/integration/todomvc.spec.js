209c4ea82a261ea9544802f4b31259d2f8394291
Vesna Petkovic <vesna.5kovic@gmail.com>
2021-12-15 20:52:52 +0100

    Todo1

---
+++ Cypress/cypress/integration/todomvc.spec.js
@@ -0,0 +1,14 @@
+///<reference types="Cypress" />
+it ('should be able to add new tot do to the list',()=>{
+    cy.visit('http://todomvc-app-for-testing.surge.sh')
+    cy.get('.new-todo', {timeout:6000}).type("Clean room{enter}");
+
+    cy.get('label').should('have.text','Clean room')
+    cy.get('.toggle').should('not.be.checked')
+    cy.get('.toggle').click()
+
+    cy.get('label').should('have.css','text-decoration-line','line-through')
+
+    cy.contains('Clear').click()
+    cy.get('.todo-list').should('not.have.descendants','li')
+})
